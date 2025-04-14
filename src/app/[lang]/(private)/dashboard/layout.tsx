e3899b68c031a403abb68f5a74217bb765b782d8
Pedro Correia <102699058+PedroCorreia73@users.noreply.github.com>
2025-04-14 17:57:21 -0300

    fix SidenavDesktop

--- src/app/[lang]/(private)/dashboard/layout.tsx
+++ src/app/[lang]/(private)/dashboard/layout.tsx
@@ -1,11 +1,15 @@
 import Header from "@/ui/header";
 import { SidenavDesktop, SidenavMobile } from "@/ui/dashboard/sidenav";
+import { SidenavWrapper } from "@/app/ui/dashboard/sidenav-wrapper";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
-      <div className="md:overflow-y-auto w-screen mt-32">{children}</div>
+      <Header isDashboard={true} />
+      <div className="flex">
+        <SidenavWrapper />
+        <div className=" md:overflow-y-auto w-screen mt-32">{children}</div>
+      </div>
       {/* <div className="md:hidden w-full flex-none md:w-64 "> */}
       <SidenavMobile />
     </>
--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,32 +1,30 @@
 import Image from "next/image";
 // import { PowerIcon, Bars3Icon } from "@heroicons/react/24/outline";
 // import { SignOut } from "@/ui/auth/signout-button";
-import { Bars3BottomLeftIcon } from "@heroicons/react/24/outline";
 import UserButton from "@/app/ui/auth/user-button";
-import { SidenavDesktop } from "./dashboard/sidenav";
-import { SidenavWrapper } from "./dashboard/sidenav-wrapper";
 
-export default function Header() {
+export default function Header({ isDashboard }: { isDashboard?: boolean }) {
   return (
-    <div className="fixed inset-x-0 top-0 z-10 border-b border-gray-200 dark:border-white/10">
-      <div className="bg-white dark:bg-gray-500">
-        <div className="flex h-14 items-center justify-between gap-8 px-4 md:px-6">
-          <div className="flex flex-row gap-5">
-            <SidenavWrapper />
-            <Image
-              priority
-              src="/assets/images/next.svg"
-              height={100}
-              width={100}
-              alt="Template"
-              className="h-10 w-20"
-            />
+    <>
+      <div className="fixed inset-x-0 top-0 z-10 border-b border-gray-200 dark:border-white/10">
+        <div className="bg-white dark:bg-gray-500">
+          <div className="flex h-14 items-center justify-between gap-8 px-4 md:px-6">
+            <div className="flex flex-row gap-5">
+              <Image
+                priority
+                src="/assets/images/next.svg"
+                height={100}
+                width={100}
+                alt="Template"
+                className={`h-10 w-20 ${isDashboard ? "ml-20" : ""}`}
+              />
+            </div>
+            <p className="text-lg">Template App</p>
+            {/* <SignOut /> */}
+            <UserButton />
           </div>
-          <p className="text-lg">Template App</p>
-          {/* <SignOut /> */}
-          <UserButton />
         </div>
       </div>
-    </div>
+    </>
   );
 }
